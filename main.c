04a0885c647feea3967051ce4b7dbe5305ef3a54
Matthew Basanta <matthew@basantamail.com>
2025-12-20 07:23:21 -0500

    scroll long text

--- main.c
+++ main.c
@@ -10,6 +10,8 @@ void SysTick_Handler(void) { s_ticks++; }
 
 uint32_t SystemCoreClock = FREQ_HZ;
 
+char* ticker_text = MCU_NAME;
+
 void SystemInit(void) { clock_init(); }
 
 int main(void) {
@@ -37,23 +39,49 @@ int main(void) {
     lcd_set_cursor(0, 0);  // Row 0, column 0
     lcd_print("Hello World!");
     lcd_set_cursor(1, 0);  // Row 1, column 0
-    lcd_print(MCU_NAME);
+    lcd_print(ticker_text);
 
     uint32_t cycles = 0;
 
-    uint32_t period = 500;  // Blink period in ms
-    uint32_t next_tick = 0;
+    uint32_t led_blink_period = 500;  // Blink period in ms
+    uint32_t next_led_blink = 0;
+
+    uint8_t scroll_ticker = strlen(ticker_text) > 16;
+    uint32_t lcd_scroll_period = 700;  // Scroll period in ms
+    uint32_t next_lcd_scroll = lcd_scroll_period;
+    uint8_t scroll_pos = 0;
 
     for (;;) {
         cycles++;
-        if (s_ticks >= next_tick) {
-            next_tick += period;
+        if (s_ticks >= next_led_blink) {
+            next_led_blink += led_blink_period;
 
             static bool on;
             printf("LED: %d, tick: %lu, cycles: %lu\r\n", on, s_ticks, cycles);
             gpio_write(LED_PIN, LED_ACTIVE_LOW ? !on : on);
             on = !on;
             cycles = 0;
+        }
+
+        if (scroll_ticker) {
+            if (s_ticks >= next_lcd_scroll) {
+                next_lcd_scroll += lcd_scroll_period;
+
+                scroll_pos++;
+                if (scroll_pos > strlen(ticker_text) - 16 + 2) {
+                    scroll_pos = 0;
+                }
+
+                lcd_set_cursor(1, 0);  // Row 1, column 0
+                for (uint8_t i = 0; i < 16; i++) {
+                    char c = ' ';
+                    uint8_t msg_index = i + scroll_pos;
+                    if (msg_index < strlen(ticker_text)) {
+                        c = ticker_text[msg_index];
+                    }
+                    lcd_data(c);
+                }
+            }
         }
     }
 
